46ed3c9f9a2e517c12dc17615adc5a50832274ff
Arthur <kingart808@gmail.com>
2019-08-08 16:52:37 -1000

    finished

--- matrix-generator.js
+++ matrix-generator.js
@@ -1,5 +1,21 @@
 function matrixGenerator(rows, columns) {
   // do work here
+  let results = [];
+  for (let i = 0; i < rows; i++) {
+    let rows = [];
+    for (let j = 0; j < columns; j++) {
+      let randomNum = Math.floor(Math.random() * 100) + 1;
+      rows.push(randomNum);
+      rows.sort(function(a, b) {
+        return a - b;
+      });
+    }
+    results.push(rows);
+    results.sort(function(a, b) {
+      return a[0] - b[0];
+    });
+  }
+  return results;
 }
 
 module.exports = matrixGenerator;
